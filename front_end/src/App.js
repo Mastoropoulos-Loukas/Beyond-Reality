420f96ca643e35639cacd633ba90200dfff8bd89
alkis_dakas <alkisdakas9@gmail.com>
2024-10-02 21:46:32 +0300

    create frontend with title

---
+++ front_end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content" >
+        <h1>Beyond Reality</h1>    
+      </div>
+    </div>
+  );
+}
+
+export default App;
